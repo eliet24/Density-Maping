e2f2cc8ab16875e1e07fd40c60570718dce6a6c4
eliet <elietin24@live.com>
2024-12-17 23:51:48 +0200

    created the map form for selecting locations according to questions and saving the data in database - updated capabilities

--- GeospatialMapping/QuestionsData.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class QuestionData(Enum):
-    FOOD= "איפה אתם עושים קניות של מוצרי מזון?"
-    FASHION = "איפה אתם קונים בגדים?"
-    NIGHTLIFE_AND_FUN = "איפה אתם יוצאים לבלות?"
-    KINDERGARTEN = "איפה הגן ילדים?"
-    SCHOOL = "איפה הבית ספר?"
-    RELIGION = "איפה הבית כנסת?"
