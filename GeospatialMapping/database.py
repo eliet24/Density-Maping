21fd9e5795499faf05720f26eb2fbfb76e485582
eliet <elietin24@live.com>
2025-01-11 22:59:49 +0200

    last updates before deploy

--- GeospatialMapping/database.py
+++ GeospatialMapping/database.py
@@ -1,9 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLite Database URL
-SQLALCHEMY_DATABASE_URL = "sqlite:///./locations.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'locations.db')}"
 
 # Create engine and session
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
