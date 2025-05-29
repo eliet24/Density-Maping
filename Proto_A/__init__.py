ec75f2d23b8b3e3d12ccd4e06e9a58da4a60b729
eliet24 <71636834+eliet24@users.noreply.github.com>
2025-05-30 01:26:34 +0300

    Delete Proto_A directory

--- Proto_A/__init__.py
+++
@@ -1,5 +0,0 @@
-from .PlaceType import PlaceMainType, PlaceSubType
-from .Place import Place
-from . import data  # Triggers registration of subtypes
-
-__all__ = ["PlaceMainType", "PlaceSubType", "Place"]
